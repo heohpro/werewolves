37f346780c5dae3ff78d1575928450337293da49
HeOH <heqingyang0126@gmail.com>
2016-01-16 22:57:46 +0800

    修改目录结构

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,14 +1,15 @@
-import {Hero} from './hero';
-import {HEROES} from './mock-heroes';
-import {Injectable} from 'angular2/core';
+/**
+ * Created by heqingyang on 16/1/8.
+ */
+
+import {Injectable} from 'angular2/core'
+import {HEROES} from './mock-heroes'
 
 @Injectable()
 export class HeroService {
-    getHeroes() {
+    getHeroes(){
         return Promise.resolve(HEROES);
     }
-
-    // See the "Take it slow" appendix
     getHeroesSlowly() {
         return new Promise<Hero[]>(resolve =>
                 setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
